72314ba6c7de5dd52cf9e5d5438b0b35acad4093
Hen-Heang <henheang15@gmail.com>
2026-03-13 17:29:26 +0900

    Initial commit

---
+++ src/main/java/com/heang/koriaibackend/KoriaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heang.koriaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoriaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoriaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heang/koriaibackend/KoriaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heang.koriaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoriaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
